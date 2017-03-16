da32be3bd3ab1a942eec2a955454f74c090abde1
Nathaly <natica61@gmail.com>
2017-03-16 17:30:17 -0500

    holi

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,48 +1,68 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
+def increase_q
+  @item.quality +=1
+end
+
+def decrease_q
+  @item.quality -=1
+end
+def q_mayor_que_0
+  if @item.name != 'Sulfuras, Hand of Ragnaros'
+    decrease_q
+  end
+end
+
+def q_menor_50
+  if @item.quality < 50
+    increase_q
+  end
+end
+
+def first_evalutation
+  if @item.name != 'Aged Brie' && @item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    if @item.quality > 0
+      q_mayor_que_0
+    end
+  else
+    if @item.quality < 50
+      @item.quality += 1
+      if @item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        if @item.sell_in < 11
+          q_menor_50
         end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
+        if @item.sell_in < 6
+          q_menor_50
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
+  end
+end
+
+def second_evaluation
+  if @item.name != 'Sulfuras, Hand of Ragnaros'
+    @item.sell_in -= 1
+  end
+  if @item.sell_in < 0
+    if @item.name != "Aged Brie"
+      if @item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if @item.quality > 0
+          if @item.name != 'Sulfuras, Hand of Ragnaros'
+            increase_q
           end
-        else
-          item.quality = item.quality - item.quality
         end
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        @item.quality = @item.quality - @item.quality
       end
+    else
+      q_menor_50
     end
+  end
+end
+
+def update_quality(items)
+  items.each do |item|
+    @item = item
+    first_evalutation
+    second_evalutation
   end
 end
 
